e6a0f2a688f56e2c886b274a20f1d80e868cb762
Nathaniel Iskandar <nathanielri22@gmail.com>
2022-03-05 13:26:49 -0800

    for doomba prime
    with updated autonomous

--- src/GPSRoutine.cpp
+++ src/GPSRoutine.cpp
@@ -10,57 +10,57 @@ using namespace std;
 
 void score130points(){
   //2
-  DriveToTargetXY(700, -1400, 27, 65);    
+  DriveToTargetXY(700, -1400, 15, 65);    
   Drivetrain.stop();
   // wait(1, sec);                               
 
   //3
-  DriveToTargetXY(790, -1000, 25, 65);
+  DriveToTargetXY(790, -1000, 15, 65);
   Drivetrain.stop();
   //wait(1, sec);
 
   //4
-  DriveToTargetXY(-710, -1000, 32, 65);
+  DriveToTargetXY(-710, -1000, 15, 65);
   Drivetrain.stop();
   //wait(1, sec);  
   
   //5
-  DriveToTargetXY(-650, 0, 25, 65);
+  DriveToTargetXY(-650, 0, 15, 65);
   Drivetrain.stop();
   //wait(1, sec);
 
   //6
-  DriveToTargetXY(730, 0, 25, 65);
+  DriveToTargetXY(730, 0, 15, 65);
   Drivetrain.stop();
   //wait(1, sec);
 
   //7
-  DriveToTargetXY(1350, 1650, 30, 65);
+  DriveToTargetXY(1350, 1650, 15, 65);
   Drivetrain.stop();
   //wait(1, sec);
 
   //8
-  DriveToTargetXY(-700, 1550, 25, 65);
+  DriveToTargetXY(-700, 1550, 15, 65);
   Drivetrain.stop();
   //wait(1, sec);
 
   //9
-  DriveToTargetXY(-230, 1100, 30, 65);
+  DriveToTargetXY(-230, 1100, 15, 65);
   Drivetrain.stop();
   //wait(1, sec);
 
   //10
-  DriveToTargetXY(530, 400, 32, 65);
+  DriveToTargetXY(530, 400, 15, 65);
   Drivetrain.stop();
   //wait(1, sec);
 
   //11
-  DriveToTargetXY(500, -1050, 32, 65);
+  DriveToTargetXY(500, -1050, 15, 65);
   Drivetrain.stop();
   //wait(1, sec);
 
   //12
-  DriveToTargetXY(1670, -1000, 25, 65);
+  DriveToTargetXY(1670, -1000, 15, 65);
   Drivetrain.stop();
   //wait(1, sec);
 
